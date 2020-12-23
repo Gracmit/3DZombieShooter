697ec003a8e6c81d2e3818685fe7574e85ed33e6
Gracmit <aleksii97@gmail.com>
2020-12-23 19:18:09 +0200

    Basic player movement implication

---
+++ 3DZombieShooter/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    private void Awake()
+    { 
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        
+        Vector3 movement = new Vector3(horizontal, 0 ,vertical);
+
+        _controller.SimpleMove(movement);
+
+
+
+    }
+}
