52750287b9e4c6a6159553cd422c4c9a858e7f72
Gracmit <aleksii97@gmail.com>
2020-12-24 14:25:46 +0200

    Added shooting. Bullets are set to inactive on collision

---
+++ 3DZombieShooter/Assets/Scripts/Bullet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("hit");
+        gameObject.SetActive(false);
+    }
+    
+}
---
+++ 3DZombieShooter/Assets/Scripts/Gun.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    [SerializeField] Transform _shootpoint;
+    [SerializeField] Bullet _bullet;
+    [SerializeField] Transform _cameraTransform;
+    [SerializeField] float _bulletSpeed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        var bullet = GameObject.Instantiate(_bullet, _shootpoint.position, _shootpoint.rotation);
+        bullet.transform.position = _shootpoint.position;
+
+        bullet.GetComponent<Rigidbody>().velocity = _cameraTransform.forward * _bulletSpeed;
+    }
+}
